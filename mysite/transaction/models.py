b14dbf04b5ef3caff5b0f488a481f73bb8898eba
Mason <masonfisher21@icloud.com>
2024-03-17 17:08:49 -0500

    Databases Partial
    This should take care of the database creation. Some portions still need to be edited so that it routes to a web page and has necessary permissions.

---
+++ mysite/transaction/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from product.models import Product
+from user.models import User
+# Create your models here.
+class Transactions(models.Model):
+    transactionID = models.AutoField(primary_key= True, unique= True)
+    productInfo = models.ForeignKey(Product, on_delete = models.CASCADE)
+    userInfo = models.ForeignKey(User, on_delete = models.CASCADE)
+    sellerID = models.ForeignKey(User, on_delete = models.CASCADE)
+    transactionTime = models.DateTimeField(auto_now = True)
+    price = models.ForeignKey(Product, to_field = Product._productID, on_delete = models.CASCADE)
+    
