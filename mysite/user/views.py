8b057fa22403abc7e6ff76f48338f0a83571f2cf
David Zetterholm <dz101@msstate.edu>
2024-03-25 15:39:25 -0500

    development push 2

--- mysite/user/views.py
+++ mysite/user/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Views for users. If you see this, we're up and running part of the way!")
