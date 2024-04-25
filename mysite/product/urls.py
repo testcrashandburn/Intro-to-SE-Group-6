d67cf4cf1fcfe64c6d99418094b58d5528ba2d07
testcrashandburn <157515912+testcrashandburn@users.noreply.github.com>
2024-04-25 18:27:47 +0000

    Clear for final push

--- mysite/product/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name = "index")
-    #path("product/", ProductView.as_view(template_name="product.html"), name = "product")
-# path("product/", TemplateView.as_view(template_name="product.html"), name="product"),
-]
