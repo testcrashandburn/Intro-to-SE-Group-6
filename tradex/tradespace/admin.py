fbeed018aecb87af2291262eb9f30523631e523d
testcrashandburn <157515912+testcrashandburn@users.noreply.github.com>
2024-04-28 22:23:16 +0000

    refresh

--- tradex/tradespace/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(CustomUser)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
